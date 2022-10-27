5d60568b39caad8856287c50415adac7fff50e8f
gsbang97 <qkdrldbs12@gmail.com>
2022-10-28 00:41:44 +0900

    합병 정렬 알고리즘

---
+++ mergy_sort.py
@@ -0,0 +1,26 @@
+def mergy_sort(slist):
+    length = len(slist)
+    if length <= 1: # 최소단위까지
+        return slist
+    left = mergy_sort(slist[:length//2])
+    right = mergy_sort(slist[length//2:])
+    
+    result = []
+    while left and right:
+        if left[0] < right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+    if left is not None:
+        result = result + left
+    if right is not None:
+        result = result + right
+    return result
+
+import random
+
+l = list()
+for _ in range(10):
+    l.append(random.randint(1,100))
+print(l)
+print(mergy_sort(l))
