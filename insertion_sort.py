7ad442ebc09ece1f6f50d81bf1f833c92f633274
gsbang97 <qkdrldbs12@gmail.com>
2022-10-28 19:19:42 +0900

    삽입정렬

---
+++ insertion_sort.py
@@ -0,0 +1,22 @@
+def sort(list):
+  length = len(list)
+
+  for i in range(1, length):
+    j = i - 1
+    tmp = list[i]
+
+    while (j >= 0) and (tmp < list[j]):
+      print(list, list[i], list[j], i, j)
+      list[j + 1] = list[j]
+      j -= 1
+    list[j + 1] = tmp
+
+
+l = [x for x in range(10)]
+import random
+
+random.shuffle(l)
+
+print(l)
+sort(l)
+print(l)
